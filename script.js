c4672580cca9e4d8931d139ad64551abf410db2a
Gulshan Soni <43094983+shiv30310@users.noreply.github.com>
2023-06-28 11:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+const output = document.getElementById("output")
+
+function resPromise(){
+	return new Promise(resolve, reject){
+		setTimeout(() => {
+			output.innerText = "Hello, world!"
+		}, 1000)
+	}
+}
+resPromise()
